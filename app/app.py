b5ad54632595f643a65347f32d76629bac9e1c6c
Grebennikov Roman <grv@dfdx.me>
2023-05-09 12:19:48 +0200

    use port 8000

--- app/app.py
+++ app/app.py
@@ -77,4 +77,4 @@ def retrieve_vector(embedding: list[float], field: str, n: int):
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8001, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)
